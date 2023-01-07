fcc761a6d57eb84794491050085614a6a5cf507a
ÂNGELO MORGADO <angelo.morgado@ubi.pt>
2023-01-07 22:00:28 +0000

    updated random sphere generation

--- Src/ParticleType.cpp
+++ Src/ParticleType.cpp
@@ -15,7 +15,8 @@ ParticleType getParticleA()
     glm::vec4 color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 
     // Generate random values
-    float radius = minRadius + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (maxRadius - minRadius)));
+    // float radius = minRadius + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (maxRadius - minRadius)));
+    float radius = maxRadius;
 
     // Position
     glm::vec3 position = createPosition(radius);
@@ -47,7 +48,8 @@ ParticleType getParticleB()
     glm::vec4 color = glm::vec4(0.68f, 0.768f, 0.153f, 1.0f); // Yellow
 
     // Generate random values
-    float radius = minRadius + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (maxRadius - minRadius)));
+    // float radius = minRadius + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (maxRadius - minRadius)));
+    float radius = maxRadius;
 
     // Position
     glm::vec3 position = createPosition(radius);
@@ -79,7 +81,8 @@ ParticleType getParticleC()
     glm::vec4 color = glm::vec4(0.72f, 0.086f, 0.086f, 1.0f);
 
     // Generate random values
-    float radius = minRadius + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (maxRadius - minRadius)));
+    // float radius = minRadius + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (maxRadius - minRadius)));
+    float radius = maxRadius;
 
     // Position
     glm::vec3 position = createPosition(radius);
@@ -97,19 +100,21 @@ ParticleType getParticleC()
 }
 
 // Auxiliary functions
-glm::vec3 createPosition(float radius)
-{
-    float theta = 2.0 * glm::pi<float>() * rand() / (float)RAND_MAX;
-    float phi = glm::pi<float>() * rand() / (float)RAND_MAX;
-
-    float positionX = radius * glm::sin(phi) * glm::cos(theta);
-    float positionY = radius * glm::sin(phi) * glm::sin(theta);
-    float positionZ = radius * glm::cos(phi);
-
-    glm::vec3 position = glm::vec3(positionX, positionY, positionZ);
-
-    return position;
+glm::vec3 createPosition(float radius) {
+    float u = rand() / (float)RAND_MAX; //random number between 0 and 1
+    float x1 = rand() / (float)RAND_MAX * 2.0 - 1.0; //random number between -1 and 1 for x.x
+    float x2 = rand() / (float)RAND_MAX * 2.0 - 1.0; //random number between -1 and 1 for x.y
+    float x3 = rand() / (float)RAND_MAX * 2.0 - 1.0; //random number between -1 and 1 for x.z
+    float mag = std::max(0.0001f, std::sqrt(x1*x1 + x2*x2 + x3*x3)); //get length of x, avoid being 0
+    float inv_mag = 1.0 / mag;
+    x1 *= inv_mag; //turn x into a direction through normalization
+    x2 *= inv_mag; //turn x into a direction through normalization
+    x3 *= inv_mag; //turn x into a direction through normalization
+    float c = std::cbrt(u) * radius; //rescale c with a cbrt to make the spherical density exactly uniform
+    // glm::vec3 position = glm::vec3(x1*c, x2*c, x3*c); //get a random point by multiplying random position from the centre with a random direction
+    return glm::vec3(x1*c, x2*c, x3*c);
 }
+
 
 glm::vec3 createVelocity(float minVelocity, float maxVelocity)
 {
