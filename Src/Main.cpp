81136243d1fc84102687b3416f013fca2e1e1026
ÂNGELO MORGADO <angelo.morgado@ubi.pt>
2023-01-08 23:57:19 +0000

    Reformated black holes

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -47,7 +47,7 @@ Camera camera;
 CameraPos cameraPos;
 
 // =============================== Global Variables =======================================
-#define N_PARTICLES 1024 // Number of particles
+#define N_PARTICLES 102400 // Number of particles
 #define PARTICLE_TEXTURE_PATH "Media/Textures/star.png" // Texture of the particles
 #define SKYBOX_PATH "Media/Skyboxes/skybox_galaxy/" // Texture to the skybox
 
@@ -65,12 +65,12 @@ int main()
     Shader objectShader("Shaders/targetShader.vert", "Shaders/targetShader.frag");
 	Shader skyboxShader("Shaders/skyboxShader.vert", "Shaders/skyboxShader.frag");
     Shader particleShader("Shaders/particlesShader.vert", "Shaders/particlesShader.frag", "Shaders/particlesShader.geom");
-    ComputeShader computeShader("Shaders/Compute/particle_shader.comp");
+    ComputeShader computeShader("Shaders/Compute/particle_shader_black_hole_movement.comp");
 
     //================================= Models ====================================================
-
 	// Load the model
     Skybox skybox(SKYBOX_PATH);
+    Model kirby("Media/Objects/Kirby.fbx");
 
     //================================= Particles =====================================
     
@@ -95,6 +95,12 @@ int main()
     default_mat.Diffuse   = glm::vec3(0.6f);
     default_mat.Specular  = glm::vec3(0.6f);
     default_mat.Shininess = 0.6f;
+
+    //================================= Scenes variables =====================================
+    glm::vec3 kirby1Pos = glm::vec3(-5.0f,0.0f,0.0f);
+    float kirby1Force = 1000.0f;
+    glm::vec3 kirby2Pos = glm::vec3(5.0f,0.0f,0.0f);
+    float kirby2Force = 1000.0f;
 
     // ====================================== Scene Setup ===============================================
     camera = Camera(
@@ -121,9 +127,56 @@ int main()
         // Draw the particles
         computeShader.use();
         computeShader.setFloat("deltaTime", camera.deltaTime);
+        computeShader.setFloat("deltaTime", camera.deltaTime);
+        computeShader.setVec3("first_black_hole_position", kirby1Pos);
+        computeShader.setVec3("second_black_hole_position", kirby2Pos);
+        computeShader.setFloat("first_g_force", kirby1Force);
+        computeShader.setFloat("second_g_force", kirby2Force);
         computeShader.execute(N_PARTICLES / 64);
         particles.Draw(particleShader, camera);
-		
+
+        // Draw the first kirby
+        objectShader.use();
+        setView(&objectShader, camera.GetViewMatrix());
+        setProjection(&objectShader, glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        setModel(
+            &objectShader, // shader
+            kirby1Pos, // translation
+            glm::vec3(0.0f, 1.0f, 0.0f), // rotation axis
+            0.0f,//(float)glfwGetTime() * 2.5f, // rotation angle
+            glm::vec3(0.015f) // scale
+        );
+        objectShader.setVec3("material.ambient", default_mat.Ambient);
+        objectShader.setVec3("material.diffuse", default_mat.Diffuse);
+        objectShader.setVec3("material.specular", default_mat.Specular);
+        objectShader.setFloat("material.shininess", default_mat.Shininess);
+        kirby.Draw(objectShader);
+
+        // Draw the second kirby
+        setModel(
+            &objectShader, // shader
+            kirby2Pos, // translation
+            glm::vec3(0.0f, 1.0f, 0.0f), // rotation axis
+            0.0f,//(float)glfwGetTime() * 2.5f, // rotation angle
+            glm::vec3(0.015f) // scale
+        );
+		kirby.Draw(objectShader);
+
+
+        // Move the kirbies positions in a circle
+        kirby1Pos = glm::vec3(
+            5.0f * cos((float)glfwGetTime() * 0.5f),
+            0.0f,
+            5.0f * sin((float)glfwGetTime() * 0.5f)
+        );
+
+        kirby2Pos = glm::vec3(
+            5.0f * cos((float)glfwGetTime() * 0.5f + glm::radians(180.0f)),
+            0.0f,
+            5.0f * sin((float)glfwGetTime() * 0.5f + glm::radians(180.0f))
+        );
+
+
         // Draw the Skybox 
         skybox.Draw(skyboxShader, camera);
 
